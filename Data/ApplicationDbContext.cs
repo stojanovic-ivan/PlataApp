1d019487c80837ee23b8a5a88647c6588034caaf
Ivan Stojanović <ivansto79@gmail.com>
2023-10-11 00:56:36 +0200

    Added DB migration

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,12 +1,24 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace PlataApp.Data;
 
-public class ApplicationDbContext : IdentityDbContext
+public class ApplicationDbContext : IdentityDbContext<IdentityUser>
 {
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-        : base(options)
-    {
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)  { }
+
+    public DbSet<Radnik> Radnici { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Radnik>(entity =>
+        {
+            // define NetoPlata as decimal with 18 digits of which 2 are decimals
+            entity.Property(e => e.NetoPlata)
+                .HasColumnType("decimal(18, 2)");
+        });
     }
+
 }
